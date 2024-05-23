318f6c10b46670f7722aa7e7a68c66a3b291c755
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-23 23:43:09 +0200

    seed continued

--- scripts/seed.js
+++ scripts/seed.js
@@ -27,6 +27,26 @@ async function main() {
 
     const token = await ethers.getContractAt('Token', '0x5FbDB2315678afecb367f032d93F642f64180aa3')
     console.log(`Token fetched: ${token.address}\n`)
+
+    let transaction = await token.connect(deployer).transfer(investor1.address, tokens(200000))
+    await transaction.wait()
+
+    transaction = await token.connect(deployer).transfer(investor2.address, tokens(200000))
+    await transaction.wait()
+
+    transaction = await token.connect(deployer).transfer(investor3.address, tokens(200000))
+    await transaction.wait()
+
+    console.log(`Fetching DAO and funding it...\n`)
+
+    const dao = await ethers.getContractAt('DAO', '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512')
+    console.log(`DAO fetched: ${dao.address}\n`)
+
+    transaction = await funder.sendTransaction({ to: dao.address, value: tokens(1000) })
+    await transaction.wait()
+    console.log(`Funded DAO with 1000 tokens\n`)
+
+    
 }
 
 // We recommend this pattern to be able to use async/await everywhere
