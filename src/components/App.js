d39c0f56936f422d74006408275b93f12c22e9c9
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-27 16:48:02 +0200

    smart contract interaction

--- src/components/App.js
+++ src/components/App.js
@@ -13,21 +13,27 @@ import DAO_ABI from '../abis/DAO.json'
 import config from '../config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
   const [dao, setDao] = useState(null)
   const [treasuryBalance, setTreasuryBalance] = useState(null)
+
+  const [proposals, setProposals] = useState([])
+  const [quorum, setQuorum] = useState(null)
 
   const [isLoading, setIsLoading] = useState(true)
 
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider);
 
     const dao = new ethers.Contract(
-      config.DAO_ADDRESS,
+      config["31337"].dao.address,
       DAO_ABI,
       provider
     )
+    console.log(dao)
     setDao(dao)
 
     let treasuryBalance = await provider.getBalance(dao.address);
@@ -38,6 +44,19 @@ function App() {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
+
+    const count = await dao.proposalCount()
+    const items = []
+
+    for (let i = 0; i < count; i++) {
+      const proposal = await dao.proposals(i + 1)
+      items.push(proposal)
+    }
+
+    setProposals(items)
+
+    const quorum = await dao.quorum()
+    setQuorum(quorum)
 
 
     setIsLoading(false)
@@ -59,9 +78,9 @@ function App() {
         <Loading />
       ) : (
         <>
-          <hr>
+          <hr/>
             <p className='text-center'><strong>Treasury Balance:</strong> {treasuryBalance}</p>
-          </hr>
+          <hr/>
         </>
       )}
     </Container>
